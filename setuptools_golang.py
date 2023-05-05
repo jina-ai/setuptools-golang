c9419c0447cd69ebbecbb783ae35c9ba7bc17b1e
Joan Fontanals Martinez <joan.martinez@jina.ai>
2023-05-05 17:20:15 +0200

    fix: remove user to get permissions

--- setuptools_golang.py
+++ setuptools_golang.py
@@ -231,7 +231,6 @@ def build_manylinux_wheels(
         (
             'docker', 'run', '--rm', '-e', f'PKG_CONFIG_PATH=/opt/python/{pythons}/lib/pkgconfig/',
             '--volume', f'{os.path.abspath("dist")}:/dist:rw',
-            '--user', f'{os.getuid()}:{os.getgid()}',
             'quay.io/pypa/manylinux2014_x86_64:latest',
             'bash', '-o', 'pipefail', '-euxc',
             SCRIPT.format(golang=golang, pythons=pythons),
